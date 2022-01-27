93cf02bb74e3b440613b42aa986a675c3cd73ed9
Andrzej Iwaniuk <andrzej.iwaniuk@live.co.uk>
2022-01-27 13:01:54 +0000

    add another query

--- app.js
+++ app.js
@@ -25,3 +25,10 @@ con.query(
     console.log(results);
   }
 );
+
+con.query("SELECT * FROM `city`", function (error, results, fields) {
+  // error will be an Error if one occurred during the query
+  // results will contain the results of the query
+  // fields will contain information about the returned results fields (if any)
+  console.log(results);
+});
