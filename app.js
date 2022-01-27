52a37c513186c0bdf3ca46627241cd3c59c4e79a
Andrzej Iwaniuk <andrzej.iwaniuk@live.co.uk>
2022-01-27 13:03:00 +0000

    add another query

--- app.js
+++ app.js
@@ -19,16 +19,14 @@ con.query(
   "SELECT * FROM `city` WHERE `Name` = ?",
   ["Kabul"],
   function (error, results, fields) {
-    // error will be an Error if one occurred during the query
-    // results will contain the results of the query
-    // fields will contain information about the returned results fields (if any)
     console.log(results);
   }
 );
 
-con.query("SELECT * FROM `city`", function (error, results, fields) {
-  // error will be an Error if one occurred during the query
-  // results will contain the results of the query
-  // fields will contain information about the returned results fields (if any)
+/*con.query("SELECT * FROM `city`", function (error, results, fields) {
   console.log(results);
+});*/
+
+con.query("SELECT * FROM `city`", function (error, results, fields) {
+  console.log(results[90]);
 });
