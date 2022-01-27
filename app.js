f883a5ddf28f092ef1532107b1e9370497b39a17
Andrzej Iwaniuk <andrzej.iwaniuk@live.co.uk>
2022-01-27 13:00:21 +0000

    add query

--- app.js
+++ app.js
@@ -7,9 +7,21 @@ var con = mysql.createConnection({
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USERNAME,
   password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
 });
 
 con.connect(function (err) {
   if (err) throw err;
   console.log("Connected!");
 });
+
+con.query(
+  "SELECT * FROM `city` WHERE `Name` = ?",
+  ["Kabul"],
+  function (error, results, fields) {
+    // error will be an Error if one occurred during the query
+    // results will contain the results of the query
+    // fields will contain information about the returned results fields (if any)
+    console.log(results);
+  }
+);
